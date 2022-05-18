c1fa045347033a4d79a6c77f2a9435b0fbd7099f
melekabbassi <abbassimelek@gmail.com>
2022-05-18 11:14:20 +0100

    test

--- src/Scolaritepi.java
+++ src/Scolaritepi.java
@@ -1,5 +1,12 @@
 public class Scolaritepi {
     public static void main(String[] args) {
+        /***** */
         new Login();
+        // new UpdateTeacherGUI(l.tt.role);
+        //new sideBarGUI();
+        // new TeacherListGUI();
+        // new GroupListGUI();
+        // new DeleteStudentGUI();
+        // new DeleteTeacherGUI();
     }
 }
