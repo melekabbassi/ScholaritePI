513ef3fc1475400e3e540e4f2a09b034fc5f29f7
melekabbassi <abbassimelek@gmail.com>
2022-05-18 11:11:16 +0100

    refresh btn doesn't work

--- src/Scolaritepi.java
+++ src/Scolaritepi.java
@@ -1,12 +1,5 @@
 public class Scolaritepi {
     public static void main(String[] args) {
-        /***** */
         new Login();
-        // new UpdateTeacherGUI(l.tt.role);
-        //new sideBarGUI();
-        // new TeacherListGUI();
-        // new GroupListGUI();
-        // new DeleteStudentGUI();
-        // new DeleteTeacherGUI();
     }
 }
